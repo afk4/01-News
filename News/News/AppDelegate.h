a907530b4427e8ca2e00059ca8474b13dd7b6b1d
唐润欣 <tangrunxin@macbook-pro.local>
2016-05-07 19:24:38 +0800

    第一次提交项目

---
+++ News/News/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  News
+//
+//  Created by 唐润欣 on 16/5/7.
+//  Copyright © 2016年 唐润欣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
